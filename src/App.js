a30a1cf13b03593bc37e02d995a4ddb64ac3b501
David Way <dway@ie.ibm.com>
2018-01-18 19:04:56 +0000

    :new: Started App component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { debounce, vibrate } from './utils';
+import { subscribeToTimer, subscribeToRoom, subscribeToPositionUpdates, setPosition } from './api';
+import Draggable from 'react-draggable';
+import iconSVG from "./print.svg"
+
+class App extends Component {
+}
+
+export default App;
