877e65b7e75aa320bfd389b2438b2fed1cb78d2b
David Way <dway@ie.ibm.com>
2018-01-18 18:59:34 +0000

    :new: App index

---
+++ server.js
@@ -0,0 +1,31 @@
+const io = require('socket.io')();
+let occupants = [];
+
+io.on('connection', (client) => {
+
+	client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+	});
+
+	client.on('subscribeToRoom', (name) => {
+    console.log('client is subscribing to Room with name ', name);
+		client.name = name;
+		client.emit('updateOccupants', io.engine.clientsCount);
+	});
+
+	client.on('setPosition', (pos) => {
+    console.log(`${client.name} is setting position to `, pos);
+		client.broadcast.emit('updatePostion', {
+			id: client.id,
+			name: client.name,
+			pos: pos
+		});
+	});
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
